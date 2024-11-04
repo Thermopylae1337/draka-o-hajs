f774308328feac58c6e03456f0f6edbcb97b3c60
Bartek <bartosz.papinski00@gmail.com>
2024-11-04 23:09:16 +0100

    listaodznak

--- Assets/_Project/Code/Models/ListaOdznak.cs
+++ Assets/_Project/Code/Models/ListaOdznak.cs
@@ -1,8 +1,29 @@
-﻿using System;
-
-public class Class1
+﻿public class ListaOdznak
 {
-	public Class1()
-	{
-	}
+    private List<Odznaka> odznaki;
+
+    public ListaOdznak()
+    {
+        odznaki = new List<Odznaka>();
+    }
+
+    public void DodajOdznake(Odznaka odznaka)
+    {
+        odznaki.Add(odznaka);
+    }
+
+    public Odznaka WyszukajOdznake(string nazwa)
+    {
+        if (odznaki.Count > 0)
+        {
+            return odznaki.Find(odznaka => odznaka.Nazwa.Equals(nazwa, StringComparison.OrdinalIgnoreCase));
+        }
+        return null;
+    }
+
+    public bool CzyOdznakaOdblokowana(string nazwa)
+    {
+        var odznaka = WyszukajOdznake(nazwa);
+        return odznaka != null && odznaka.CzyOdblokowana();
+    }
 }
