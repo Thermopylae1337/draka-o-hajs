ca2037a4bd0403d468787ab0c45cf55d37dc1182
Bartek <bartosz.papinski00@gmail.com>
2024-11-04 23:06:10 +0100

    klasy odznaka, pytanie, listaodznak

---
+++ Assets/_Project/Code/Models/Odznaka.cs
@@ -0,0 +1,23 @@
+﻿public class Odznaka
+{
+    public string Nazwa { get; private set; }
+    public bool StatusOdblokowania { get; private set; }
+    public string WarunekOdblokowania { get; private set; }
+
+    public Odznaka(string nazwa, string warunekOdblokowania)
+    {
+        Nazwa = nazwa;
+        WarunekOdblokowania = warunekOdblokowania;
+        StatusOdblokowania = false;
+    }
+
+    public void Odblokuj()
+    {
+        StatusOdblokowania = true;
+    }
+
+    public bool CzyOdblokowana()
+    {
+        return StatusOdblokowania;
+    }
+}
---
+++ Assets/_Project/Code/Models/Pytanie.cs
@@ -0,0 +1,35 @@
+﻿public class Pytanie
+{
+    public Pytanie()
+    {
+
+        public string Tresc { get; private set; }
+    private List<string> poprawneOdpowiedzi = new List<string>();
+    private Dictionary<char, string> odpowiedzi;
+
+
+    public Pytanie(string Tresc, List<string> poprawneOdpowiedzi, string odpA, string odpB, string odpC, string odpD)
+    {
+        this.Tresc = Tresc;
+        this.poprawneOdpowiedzi = poprawneOdpowiedzi; // podane jako lista poprawne warianty odpowiedzi
+
+        odpowiedzi = new Dictionary<char, string>
+                {
+                    {'A', odpA},
+                    {'B', odpB},
+                    {'C', odpC},
+                    {'D', odpD}
+                };
+    }
+
+    public bool czyPoprawna(string odpowiedz)
+    {
+        return poprawneOdpowiedzi.Contains(odpowiedz.Trim().ToLower());
+    }
+
+    public string Podpowiedz()
+    {
+        return $"A: {odpowiedzi['A']}, B: {odpowiedzi['B']}, C: {odpowiedzi['C']}, D: {odpowiedzi['D']}";
+    }
+}
+}
