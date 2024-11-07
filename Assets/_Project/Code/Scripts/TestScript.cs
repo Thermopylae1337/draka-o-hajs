c7eb097915dde504cee5962d92cb6e661c2d795f
Adrian <adisiwek@gmail.com>
2024-11-07 20:47:58 +0100

    Dodanie przejsc pomiedzy scenami, naprawa bugow

---
+++ Assets/_Project/Code/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestScript : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("etap-pytania");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
