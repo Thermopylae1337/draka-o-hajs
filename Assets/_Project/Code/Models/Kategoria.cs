c8a146f62cd7330263b5dfe96c9ecc5725290cba
VogimKof <imkofigvogel@gmial.com>
2024-11-08 16:07:13 +0100

    Dodanie klas: Pytanie, Kategoria, ListaKategorii

---
+++ Assets/_Project/Code/Models/Kategoria.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+public class Kategoria
+{
+    public string Nazwa { get; }
+    [JsonProperty("Lista Pytań", Order = 2)]
+    public List<Pytanie> ListaPytań;
+
+    public Kategoria(string nazwa)
+    {
+        Nazwa = nazwa;
+        ListaPytań = new List<Pytanie>();
+    }
+
+    [JsonConstructor]
+    public Kategoria(string nazwa, List<Pytanie> lista)
+    {
+        this.Nazwa = nazwa;
+        this.ListaPytań = lista;
+    }
+
+    public void DodajPytanieDoListy(Pytanie pytanie)
+    {
+        if (pytanie == null)
+        {
+            return;
+        }
+        ListaPytań.Add(pytanie);
+    }
+
+    public Pytanie LosujPytanie()
+    {
+        Random random = new Random();
+        Pytanie question = ListaPytań.Count == 0 ? null : ListaPytań[random.Next(ListaPytań.Count)];
+
+        ListaPytań.Remove(question);
+        return question;
+
+    }
+
+    public void Serializuj(string sciezka)
+    {
+        JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            Formatting = Newtonsoft.Json.Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore
+        };
+        string json = JsonConvert.SerializeObject(this, settings);
+        File.WriteAllText(sciezka, json);
+    }
+
+    public static Kategoria Deserializuj(string sciezka)
+    {
+        if (!File.Exists(sciezka))
+        {
+            throw new FileNotFoundException("Nie znaleziono pliku.", sciezka);
+        }
+        string json = File.ReadAllText(sciezka);
+        return JsonConvert.DeserializeObject<Kategoria>(json);
+    }
+
+}
---
+++ Assets/_Project/Code/Models/Pytanie.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Pytanie
+{
+    public string Tresc { get; private set; }
+    [JsonProperty("Poprawne Odpowiedzi", Order = 2)]
+    private List<string> poprawneOdpowiedzi = new List<string>();
+    [JsonProperty("Odpowiedzi", Order = 3)]
+    private Dictionary<char, string> odpowiedzi;
+
+
+    public Pytanie(string Tresc, List<string> poprawneOdpowiedzi, string odpA, string odpB, string odpC, string odpD)
+    {
+        this.Tresc = Tresc;
+        this.poprawneOdpowiedzi = poprawneOdpowiedzi; // podane jako lista poprawne warianty odpowiedzi
+
+        odpowiedzi = new Dictionary<char, string>
+        {
+            {'A', odpA},
+            {'B', odpB},
+            {'C', odpC},
+            {'D', odpD}
+        };
+    }
+
+    public bool czyPoprawna(string odpowiedz)
+    {
+        return poprawneOdpowiedzi.Contains(odpowiedz.Trim().ToLower());
+    }
+
+    public string Podpowiedz()
+    {
+        return $"A: {odpowiedzi['A']}, B: {odpowiedzi['B']}, C: {odpowiedzi['C']}, D: {odpowiedzi['D']}";
+    }
+
+    public void Serializuj(string sciezka)
+    {
+        JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            Formatting = Newtonsoft.Json.Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore
+        };
+        string json = JsonConvert.SerializeObject(this, settings);
+        File.WriteAllText(sciezka, json);
+    }
+
+    public static Pytanie Deserializuj(string sciezka)
+    {
+        if (!File.Exists(sciezka))
+        {
+            throw new FileNotFoundException("Nie znaleziono pliku.", sciezka);
+        }
+        string json = File.ReadAllText(sciezka);
+        return JsonConvert.DeserializeObject<Pytanie>(json);
+    }
+}
+
