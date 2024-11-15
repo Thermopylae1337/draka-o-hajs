06d8654b423bd62aabfe2b46c6378d97333e7ea5
ThePhaseless <kukubaorch@gmail.com>
2024-11-16 00:45:01 +0100

    switch to constants

--- Assets/_Project/Code/Models/Utils.cs
+++ Assets/_Project/Code/Models/Utils.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 public static class Utils
 {
-    public readonly static int START_MONEY = 10000;
-    public readonly static string TEAM_DEFAULT_NAME = "New Team";
+    public const int START_MONEY = 10000;
+    public const string TEAM_DEFAULT_NAME = "New Team";
     public const int ROUNDS_LIMIT = 7;
 
 
