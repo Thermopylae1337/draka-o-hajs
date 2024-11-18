05aa7a555fcb2be0d4a23806b6b8d705e8c6f7a5
Bartek <bartosz.papinski00@gmail.com>
2024-11-18 21:56:43 +0100

    BadgeList change

--- Assets/_Project/Code/Models/BadgeList.cs
+++ Assets/_Project/Code/Models/BadgeList.cs
@@ -22,14 +22,14 @@ public class BadgeList
     {
         if (badges.Count > 0)
         {
-            return badges.Find(badge => badge.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            return badges?.Find(badge => badge.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
         }
         return null;
     }
 
     public bool IsBadgeUnlocked(string name)
     {
-        var badge = FindBadge(name);
+        var badge? = FindBadge(name);
         return badge != null;
     }
 }
