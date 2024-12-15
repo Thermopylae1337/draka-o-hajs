283541358cbb9730f599cc704ef0a7b04f1c85c0
ThePhaseless <kukubaorch@gmail.com>
2024-12-15 02:52:07 +0100

    add ip and spinner

--- Assets/_Project/Code/Scripts/TeamCreator/TeamCreatorController.cs
+++ Assets/_Project/Code/Scripts/TeamCreator/TeamCreatorController.cs
@@ -1,6 +1,8 @@
 using Assets._Project.Code.Models;
 using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,14 +10,32 @@ using UnityEngine.UI;
 public class TeamCreatorController : NetworkBehaviour // dodac back to main menu
 {
     public TMP_InputField inputField;
+    public GameObject spinner;
+    public GameObject ipField;
+    public GameObject errorMessage;
     public static string chosenTeamName;
     public Button returnButton;
     public Button submitButton;
+    TMP_InputField tMP_InputField;
 
     private void Start()
     {
+        tMP_InputField = ipField.GetComponentInChildren<TMP_InputField>();
         submitButton.onClick.AddListener(OnInputSubmit);
         returnButton.onClick.AddListener(OnReturnToMenu);
+
+        if (MainMenuController.lobbyType != LobbyTypeEnum.Join)
+        {
+            ipField.SetActive(false);
+        }
+        else
+        {
+            tMP_InputField.text = NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address;
+            tMP_InputField.onValueChanged.AddListener((newValue) =>
+            {
+                errorMessage.SetActive(false);
+            });
+        }
     }
 
     public void OnInputSubmit()
@@ -29,6 +49,7 @@ public class TeamCreatorController : NetworkBehaviour // dodac back to main menu
             //dodanie zapisu, odczytu teamu?
             // jakis check na zakazane słowa? XDD
             inputField.interactable = false;
+            tMP_InputField.interactable = false;
             chosenTeamName = userInput;
             StartGame();
         }
@@ -43,6 +64,26 @@ public class TeamCreatorController : NetworkBehaviour // dodac back to main menu
         SceneManager.LoadScene("MainMenu");
     }
 
+    private void HandleErrorClient(ulong value)
+    {
+        RemoveClientHandlers();
+        errorMessage.SetActive(true);
+
+    }
+
+    private void HandleSuccessClient(ulong value)
+    {
+        RemoveClientHandlers();
+    }
+    void RemoveClientHandlers()
+    {
+        spinner.SetActive(false);
+        NetworkManager.Singleton.OnClientConnectedCallback -= HandleSuccessClient;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= HandleErrorClient;
+        inputField.interactable = true;
+        tMP_InputField.interactable = true;
+    }
+
     private void StartGame()
     {
         switch (MainMenuController.lobbyType)
@@ -53,7 +94,14 @@ public class TeamCreatorController : NetworkBehaviour // dodac back to main menu
                 _ = NetworkManager.Singleton.SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
                 break;
             case LobbyTypeEnum.Join:
-                _ = NetworkManager.StartClient();
+                NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ipField.GetComponentInChildren<TMP_InputField>().text;
+
+                NetworkManager.Singleton.OnClientDisconnectCallback += HandleErrorClient;
+                NetworkManager.Singleton.OnClientConnectedCallback += HandleSuccessClient;
+
+                spinner.SetActive(true);
+                bool success = NetworkManager.StartClient();
+
                 // SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
                 break;
             default:
