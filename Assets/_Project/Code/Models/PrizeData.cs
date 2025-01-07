a36b39cafa4be782dd144d5254e7767804ff6275
VogimKof <imkofigvogel@gmial.com>
2025-01-07 10:26:42 +0100

    dokumantacja v2

--- Assets/_Project/Code/Models/PrizeData.cs
+++ Assets/_Project/Code/Models/PrizeData.cs
@@ -1,13 +1,30 @@
 using System;
 using Unity.Netcode;
 
+/// <summary>
+/// Klasa reprezentująca nagrodę uzyskaną przez drużynę.
+/// </summary>
 [Serializable]
 public struct PrizeData : INetworkSerializable
 {
+    /// <summary>
+    /// Zmienna przechowująca nazwę drużyny.
+    /// </summary>
     public string teamName;
+    /// <summary>
+    /// Zmienna przechowująca pieniądze wylosowane przez drużynę.
+    /// </summary>
     public int money;
+    /// <summary>
+    /// Zmienna przechowująca odznakę uzyskaną przez drużynę
+    /// </summary>
     public string badge;
 
+    /// <summary>
+    /// Metoda służąca do serializacji obiektu.
+    /// </summary>
+    /// <typeparam name="T">Parametr będący typem klasy realizującej interfejs IReaderWriter.</typeparam>
+    /// <param name="serializer">Zmienna reprezentująca obiekt klasy BufferSerializer<T>.</param>
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
         teamName ??= string.Empty;
--- Assets/_Project/Code/Models/PrizeDataList.cs
+++ Assets/_Project/Code/Models/PrizeDataList.cs
@@ -1,11 +1,22 @@
 using System;
 using Unity.Netcode;
 
+/// <summary>
+/// Klasa służąca do przechowywania nagród uzyskanych przez drużynę.
+/// </summary>
 [Serializable]
 public struct PrizeDataList : INetworkSerializable
 {
+    /// <summary>
+    /// Zmienna przechowywująca listę nagród uzyskanych przez drużynę.
+    /// </summary>
     public PrizeData[] prizes;
 
+    /// <summary>
+    /// Metoda służąca do serializacji klasy.
+    /// </summary>
+    /// <typeparam name="T">Parametr będący typem klasy realizującej interfejs IReaderWriter.</typeparam>
+    /// <param name="serializer">Zmienna reprezentująca obiekt klasy BufferSerializer<T>.</param>
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
         int count = prizes?.Length ?? 0;
